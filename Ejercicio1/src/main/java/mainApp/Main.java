581c49d337dbd3aa203c8d86894bf8d92e04fe1a
juanXxue6 <juanxue6@gmail.com>
2022-11-08 09:50:28 +0100

    commit cadena main

--- Ejercicio1/src/main/java/mainApp/Main.java
+++ Ejercicio1/src/main/java/mainApp/Main.java
@@ -12,7 +12,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		DB_model model = new DB_model("jdbc:mysql://192.168.1.190:3306","remote","Saraysofia2010*");
+		DB_model model = new DB_model("jdbc:mysql://192.168.1.190:3306","remote","");
 
 		VistaGestion vg = new VistaGestion();
 		VistaCliente vc = new VistaCliente();
